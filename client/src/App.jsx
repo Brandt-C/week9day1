56a1884cdab8064d26b6a795dd1f5d6c2a5ae5bf
SomeCoolStudio <47907012+SomeCoolStudio@users.noreply.github.com>
2024-04-17 22:45:53 -0700

    Week 9 Day 2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,30 @@
 import axios from 'axios';
 
 import BackgroundVideo from './components/BackgroundVideo';
-import { useState, useEffect } from 'react';
 import LoginForm from './components/LoginForm';
 import Header from './components/Header';
+import TestUseState from './components/TestUseState';
 
 
 export default function App() { 
 
 
-  const fetchAPI = async () =>{
-    const response = await axios.get("http://127.0.0.1:5000/api/users");
-    console.log(response.data.users);
-  };
+  // const fetchAPI = async () =>{
+  //   const response = await axios.get("http://127.0.0.1:5000/api/users");
+  //   console.log(response.data.users);
+  // };
 
-  useEffect(() => {
-    fetchAPI();
-  }, []);
+  // useEffect(() => {
+  //   fetchAPI();
+  // }, []);
 
   return (
     <>
         In the beginning... 
         <BackgroundVideo />
-        <Header/>
+        <Header />
         <LoginForm />
+        <TestUseState />
     </>
   )
 }
